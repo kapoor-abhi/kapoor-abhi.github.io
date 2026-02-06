69edc852724d1800662196b8684e1b8af750ec20
Abhishek Kapoor <909abhishekkapoor@gmail.com>
2026-02-06 14:32:40 +0530

    first commit

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const images = [
   "images/hero1.jpg",
   "images/hero2.jpg",
-  "images/hero3.jpg",
-  "images/hero4.jpg"
+  "images/hero3.jpg"
 ];
 
 let index = 0;
@@ -11,4 +10,4 @@ const hero = document.getElementById("hero-img");
 setInterval(() => {
   index = (index + 1) % images.length;
   hero.src = images[index];
-}, 5000); // change every 5s
+}, 5000);
