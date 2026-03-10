329a28ea93a5863e5c0af602bd72da4772f828e6
Abhishek Kapoor <909abhishekkapoor@gmail.com>
2026-03-10 22:53:39 +0530

    update readme

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
-const images = [
-  "images/photo1.png",
-  "images/photo5.png",
-  "images/photo3.png",
-  "images/photo4.png",
-  "images/photo22.png"
+document.addEventListener("DOMContentLoaded", () => {
+  const rotImages = document.querySelectorAll('.rot-img');
 
-];
-
-let index = 0;
-const hero = document.getElementById("hero-img");
-
-setInterval(() => {
-  index = (index + 1) % images.length;
-  hero.src = images[index];
-}, 5000);
+  if (rotImages.length > 0) {
+    let index = 0;
+    setInterval(() => {
+      rotImages[index].classList.remove('active');
+      index = (index + 1) % rotImages.length;
+      rotImages[index].classList.add('active');
+    }, 4500); // Change image every 4.5 seconds
+  }
+});
